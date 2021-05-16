731495043e39a208495a186b59fb2867951e3a61
Bimal Boby <bimalboby007@gmail.com>
2021-05-16 05:02:44 -0800

    some errors are there .the video is loaded but the face markings not visible,coding completed

--- script.js
+++ script.js
@@ -3,6 +3,16 @@
 
 
 const video=document.getElementById("video")
+
+
+Promise.all([
+
+    faceapi.nets.tinyFaceDetector.loadFromUri('models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('models')
+
+]).then(beginVideo)
+
 async function beginVideo(){
     let stream =null;
     try{
@@ -14,4 +24,23 @@ async function beginVideo(){
         console.log(err);
     }
 }
-beginVideo()
+video.addEventListener('play',()=>{
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const dim={width:video.width,heigth:video.heigth}
+    faceapi.matchDimension(canvas,dim)
+
+    setInterval(async()=>{
+
+     const detections= await  faceapi.detectAllFaces(video,new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections= faceapi.resizeResults(detections,dim)
+    canvas.getContext("2d").clearRect(0,0,canvas.width,canvas.heigth)
+    faceapi.draw.drawDetections(canvas,resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas,resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas,resizedDetections)
+    
+
+    },100)
+
+
+})
