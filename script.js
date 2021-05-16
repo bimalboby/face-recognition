20271201dce72f4fa34acb9b0534b82fff9bf33f
Bimal Boby <bimalboby007@gmail.com>
2021-05-16 04:11:12 -0800

    webcam loaded

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //Author : Bimal Boby
+
+
+
+const video=document.getElementById("video")
+async function beginVideo(){
+    let stream =null;
+    try{
+         stream= await navigator.mediaDevices.getUserMedia({audio:false,video:true})
+         video.srcObject=stream
+    }
+    catch(err){
+        alert("unable to find device")
+        console.log(err);
+    }
+}
+beginVideo()
